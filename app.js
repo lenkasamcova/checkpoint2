37bea4021663e5071a7237e9a8334d84ebbb69b9
lenkasamcova <73078969+lenkasamcova@users.noreply.github.com>
2021-11-30 10:58:19 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+
+import '../bootstrap'
+
+// components
+import FlashMessage from './components/FlashMessage.vue'
+import Comment from './components/Comment.vue'
+
+// vue
+const app = new Vue({
+	el: '#app',
+	components: {
+		FlashMessage, 
+		Comment
+	},
+	mounted() {
+		if (location.hash) {
+			setTimeout(() => {
+				let el = location.hash.replace('scroll-to-', '')
+				document
+					.querySelector(el)
+					.scrollIntoView({ behavior: 'smooth' })
+			}, 150)
+		}
+	}
+})
